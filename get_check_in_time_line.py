c7b697ea650a52f27a83146bd59a66dc165cb41d
zouyaowu <zouyaowu001@163.com>
2018-08-08 22:32:52 +0800

    修改each_workload

--- get_check_in_time_line.py
+++ get_check_in_time_line.py
@@ -148,7 +148,8 @@ def each_workload(con=None,cur=None,table=None,paging=True):
     cur.execute('select tester from check_in_datas group by tester')
     page=Page("工作量")
     line = Line()
-    attr = []
+    attr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
+            27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
     v = []
     for tester in cur.fetchall():
         cur.execute('select date_test from check_in_datas where tester like ?', (tester[0],))
